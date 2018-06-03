f4381efa971bd62ddc26d3d6ef2e3862b3c31b1b
satish <svdesai75@gmail.com>
2018-06-03 16:04:59 -0500

    Enforce consistency of granulaity specifier
    + For solar-edge and neurio client interfaces to use same consistent specifiers for granularity (frequency) of time-series data
    + Raise exceptions on (some) invalid inputs

--- interfaces/NeurioClient.py
+++ interfaces/NeurioClient.py
@@ -2,9 +2,6 @@
 import neurio
 import pandas as pd
 
-
-#TODO make consistent granularity specifiers
-
 class NeurioClient:
 
     def __init__(self,cfg,monitorID):
@@ -17,12 +14,27 @@ class NeurioClient:
         tp = neurio.TokenProvider(key=clientID, secret=secret)
         self.client=neurio.Client(token_provider=tp)
 
-    def fetchConsumptionData(self, start, end, granularity):
+    def fetchConsumptionData(self, start, end, timeUnit):
 
+        timeSpan=end - start
+        if timeUnit == 'QUARTER_OF_AN_HOUR' and timeSpan > 1:
+            raise Exception('Granularity of QUARTER_OF_AN_HOUR does not support time span > 1 day')
+
+        granularityInfo={}
+        granularityInfo['QUARTER_OF_AN_HOUR'] = ('minutes', 15)
+        granularityInfo[              'HOUR'] = (  'hours',  1)
+        granularityInfo[               'DAY'] = (   'days',  1)
+        granularityInfo[             'MONTH'] = ( 'months',  1)
+        granularityInfo[              'YEAR'] = (  'years',  1)
+
+        if timeUnit not in granularityInfo.keys():
+            raise Exception ('Invalid time_unit %s' % timeUnit)
+        granularity,frequency = granularityInfo[timeUnit]
         raw=self.client.get_samples_stats(sensor_id=self.monitorID,
                                           start=start.isoformat(),
                                           end=end.isoformat(),
-                                          granularity=granularity)
+                                          granularity=granularity,
+                                          frequency=frequency)
 
         df=pd.DataFrame(raw)
         df.start = pd.to_datetime(df.start).dt.tz_localize('America/Chicago').dt.tz_convert("America/Chicago")
--- interfaces/SolarEdgeClient.py
+++ interfaces/SolarEdgeClient.py
@@ -11,8 +11,13 @@ class SolarEdgeClient:
 
         self.client =  solaredge.Solaredge(key)
 
-    def getProductionData(self, start, end):
-        siteEnergy=self.client.get_energy_details_dataframe(site_id=self.siteID,start_time=start,end_time=end,time_unit="HOUR")
+    def getProductionData(self, start, end, timeUnit):
+
+        validGranularities=['QUARTER_OF_AN_HOUR', 'HOUR', 'DAY', 'WEEK', 'MONTH', 'YEAR']
+        if timeUnit not in validGranularities:
+            raise Exception('Invalid time_unit %s' % timeUnit)
+
+        siteEnergy=self.client.get_energy_details_dataframe(site_id=self.siteID, start_time=start, end_time=end, time_unit=timeUnit)
         siteEnergy.reset_index(level=0, inplace=True)
         siteEnergy.date=pd.to_datetime(siteEnergy.date)
         return siteEnergy
