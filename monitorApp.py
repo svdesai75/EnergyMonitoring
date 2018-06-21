dc5282f1ccc5373ef243167efdf1ae3dc8669154
satish <svdesai75@gmail.com>
2018-06-20 22:00:57 -0500

    Initial commit of bokeh app for visualizing energy production and consumption

---
+++ monitorApp.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+from dataModel import *
+
+
+from bokeh.plotting import figure, output_file, show
+from bokeh.layouts import gridplot
+
+from tornado.ioloop import IOLoop
+from bokeh.application.handlers import FunctionHandler
+from bokeh.application import Application
+from bokeh.server.server import Server
+
+from bokeh.models.widgets import DatePicker
+
+io_loop = IOLoop.current()
+
+def monitoringApp(doc):
+    engine=createDBEngine()
+    session=dbConnect(engine)
+    startTime=pd.to_datetime('2018-04-21 08:00:00')
+    endTime=pd.to_datetime('2018-05-20 01:00:00')
+
+    plots=[]
+
+    rentalUnits=session.query(RentalUnit).all()
+    for unit in rentalUnits:
+        print("===================")
+        print (unit.unitName)
+        print (unit.propertyID)
+        #time_unit="QUARTER_OF_AN_HOUR"
+        time_unit="HOUR"
+        combined=unit.getEnergyData(startTime, endTime, time_unit)
+        print (combined.head())
+        print (combined.producedEnergy.sum())
+        unitPlot=figure(title=unit.unitName,x_axis_type="datetime",plot_width=1000,plot_height=400)
+        unitPlot.line(combined.date, combined.producedEnergy,legend="Solar Production",color="blue")
+        unitPlot.line(combined.date, combined.consumedEnergy,legend="Usage",color="red")
+        plots.append(unitPlot)
+        print(plots)
+
+    outGrid=gridplot(plots,ncols=1)
+    #curdoc().add_root(outGrid)
+    doc.add_root(outGrid)
+
+#curdoc().add_root(column(button, p))
+#show(outGrid)
+bokeh_app = Application(FunctionHandler(monitoringApp))
+
+server = Server({'/': bokeh_app}, io_loop=io_loop)
+server.start()
+
+if __name__ == '__main__':
+    print('Opening Bokeh application on http://localhost:5006/')
+    io_loop.add_callback(server.show, "/")
+    io_loop.start()
