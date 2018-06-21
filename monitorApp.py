fd511414cd4d585a3c89f662a5b84fa10d771211
satish <svdesai75@gmail.com>
2018-06-20 22:47:57 -0500

    Updates toward interactivity:
    + add forms for selecting rental unit and start/end dates
    + create list of known units for above
    + start tweaking layout

--- monitorApp.py
+++ monitorApp.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 from dataModel import *
 
-
-from bokeh.plotting import figure, output_file, show
-from bokeh.layouts import gridplot
+from bokeh.plotting import figure
+from bokeh.layouts import  row, column
 
 from tornado.ioloop import IOLoop
 from bokeh.application.handlers import FunctionHandler
@@ -11,12 +10,20 @@ from bokeh.application import Application
 from bokeh.server.server import Server
 
 from bokeh.models.widgets import DatePicker
+from bokeh.models.widgets.inputs import AutocompleteInput
 
 io_loop = IOLoop.current()
 
+engine = createDBEngine()
+session = dbConnect(engine)
+
+rentalUnits = [unit.unitName for unit in session.query(RentalUnit).all()]
+rentalUnitSelector=AutocompleteInput(title="Rental Unit",completions=rentalUnits)
+
+startDatePicker=DatePicker(title="Start Date", min_date="2018-04-01")
+endDatePicker  =DatePicker(title="End Date", min_date="2018-04-01")
+
 def monitoringApp(doc):
-    engine=createDBEngine()
-    session=dbConnect(engine)
     startTime=pd.to_datetime('2018-04-21 08:00:00')
     endTime=pd.to_datetime('2018-05-20 01:00:00')
 
@@ -38,12 +45,12 @@ def monitoringApp(doc):
         plots.append(unitPlot)
         print(plots)
 
-    outGrid=gridplot(plots,ncols=1)
+    widgets=column(rentalUnitSelector,startDatePicker, endDatePicker)
+    plotsCol=column(plots)
+    layout=row(widgets,plotsCol)#(plots,ncols=1)
     #curdoc().add_root(outGrid)
-    doc.add_root(outGrid)
+    doc.add_root(layout)
 
-#curdoc().add_root(column(button, p))
-#show(outGrid)
 bokeh_app = Application(FunctionHandler(monitoringApp))
 
 server = Server({'/': bokeh_app}, io_loop=io_loop)
