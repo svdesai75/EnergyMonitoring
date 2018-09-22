60982fdb480bcb90ffd282a02c291291fdef1f2b
satish <svdesai75@gmail.com>
2018-09-22 17:32:55 -0500

    Initial commit of script to initialize Data Cache

---
+++ bin/initDataCache.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from dataCache import DataCache
+import logger
+
+logger.info("Initializing Data Cache")
+dataCache=DataCache()
+dataCache.initDataCache()
+
+logger.info("Done initializing Data Cache")
+
