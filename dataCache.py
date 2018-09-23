d58da7e96f4fb93a95d06bc3f8d900154114b91e
satish <svdesai75@gmail.com>
2018-09-23 16:54:34 -0500

    + Drop DB in initDB method + add compound index to deviceData collection

--- dataCache.py
+++ dataCache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from energyMonitorConfig import *
-from pymongo import MongoClient, 
+from pymongo import MongoClient
 
 class DataCache:
     def __init__(self):
@@ -14,15 +14,23 @@ class DataCache:
         return client
 
     def initDataCache(self):
+        self.client.drop_database('dataCache')
+        self.db=self.client['dataCache']
+
         ## create collection for device data
-        self.db.create_collection(name='deviceData')
+        deviceData=self.db.create_collection(name='deviceData')
+        deviceData.create_index([("deviceType",1), ("deviceID",1),("timeUnit",1),("timeStamp", 1)], unique=True)
+
         ## create collection for billing data
         self.db.create_collection(name='billingData')
 
     def addDeviceDataSingleRecord(self,deviceType,deviceID, timeUnit,timeStamp, record):
+        deviceData=self.db['deviceData']
         #check if record with time stamp exists:
         #  if yes, print warning message and skip
         #  #if no, insert
+        toInsert={"deviceType":deviceType, "deviceID":deviceID, "timeUnit":timeUnit,"timeStamp": timeStamp, "data":record}
+        deviceData.insert_one(toInsert)
 
     def addDeviceDataMultipleRecords(self, deviceType, deviceID, timeUnit, timeStampKey, records):
         for record in records:
