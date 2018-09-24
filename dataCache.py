28a56d2f68b1d8b4e0b5f3216e7368fe66e858ba
satish <svdesai75@gmail.com>
2018-09-23 20:08:13 -0500

    + + add function to read local device data + add function to get device-data latest timestamp + streamline sme comments

--- dataCache.py
+++ dataCache.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from energyMonitorConfig import *
 from pymongo import MongoClient
+import pymongo
 
 class DataCache:
     def __init__(self):
@@ -26,9 +27,7 @@ class DataCache:
 
     def addDeviceDataSingleRecord(self,deviceType,deviceID, timeUnit,timeStamp, record):
         deviceData=self.db['deviceData']
-        #check if record with time stamp exists:
-        #  if yes, print warning message and skip
-        #  #if no, insert
+        # TODO: appropriate error handling for duplicate data (catch exception?)
         toInsert={"deviceType":deviceType, "deviceID":deviceID, "timeUnit":timeUnit,"timeStamp": timeStamp, "data":record}
         deviceData.insert_one(toInsert)
 
@@ -38,6 +37,19 @@ class DataCache:
             self.addDeviceDataSingleRecord(deviceType,deviceID,timeUnit,timeStamp,record)
 
     ##function to fetch deviceData for given timespan
-    ##function to get latest timestamp
+    def getDeviceData(self, deviceType, deviceID, timeUnit, start, end):
+        deviceData=self.db['deviceData'].find({'deviceID':deviceID,  'deviceID':deviceID, 'timeUnit':timeUnit, 'timeStamp':{'$gte':start, '$lte':end}})
+        return [d['data'] for d in deviceData]
+
+    ##in principle, we should have a get-earliest timestamp too, but in the current model,
+    ##  we are building up a history, so that seems unlikely to be needed
+    def getDeviceLatestTimestamp(self,deviceType, deviceID, timeUnit):
+        filter={'deviceID':deviceID,  'deviceID':deviceID, 'timeUnit':timeUnit}
+        sort=[('timeStamp',pymongo.DESCENDING)]
+        queryResult=self.db['deviceData'].\
+            find_one(filter=filter, sort=sort,projection=['timeStamp'])['timeStamp']
+
+        return queryResult
+
 
     #function to add billing address
