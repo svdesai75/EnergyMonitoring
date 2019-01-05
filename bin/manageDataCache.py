d6acc0b6917096841cd72e195efbb30cf0b50dcc
satish <svdesai75@gmail.com>
2019-01-05 15:51:45 -0600

    Initial commit of script to perform management of DataCache

---
+++ bin/manageDataCache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from dataCache import DataCache
+import logger
+import argparse, sys
+
+logger.info("Loading Data Cache")
+dataCache=DataCache()
+
+def listDevices():
+    print ("Listing Devices")
+    deviceList=dataCache.getDeviceList()
+    for d in deviceList:
+        print(d)
+
+def actionNotSupported():
+    print ("Action not supported")
+
+parser=argparse.ArgumentParser(prog=sys.argv[0], description="Manage/inspect local data Cache")
+subParser=parser.add_subparsers(title="Action",dest="action")
+subParser.required=True
+
+subParsers={}
+subParsers[         "initDB"]={"help":               "Re-initialize DB","func":dataCache.initDataCache}
+subParsers[    "listDevices"]={"help":                   "List devices","func":listDevices}
+subParsers["clearAllDevices"]={"help":     "Clear data for all devices","func":actionNotSupported}
+subParsers[    "clearDevice"]={"help": "Clear data for specific device","func":actionNotSupported}
+subParsers[     "viewDevice"]={"help":  "View data for specific device","func":actionNotSupported}
+
+for cmd in subParsers:
+    help=subParsers[cmd]["help"]
+    sp=subParser.add_parser(cmd, help=help)
+
+x=parser.parse_args()
+subParsers[x.action]['func']()
+
+
+#dataCache.initDataCache()
+
+
