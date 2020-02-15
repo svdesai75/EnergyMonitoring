0ec1b7be6e640f5779f12f6fc19e77def03c8c36
satish <svdesai75@gmail.com>
2020-02-15 17:46:12 -0600

    style fixes

--- dataCache.py
+++ dataCache.py
@@ -11,20 +11,20 @@ import logger
 
 class DataCache:
     def __init__(self):
-        cfg=loadCfg()
-        self.client=self.getDataCacheClient(cfg)
-        self.db=self.client['dataCache']
-        print (self.db)
+        cfg = loadCfg()
+        self.client = self.getDataCacheClient(cfg)
+        self.db = self.client['dataCache']
+        print(self.db)
 
     def getDataCacheClient(self,cfg):
-        uri=cfg['devicedata']['dburi']
-        client=MongoClient(uri)
+        uri = cfg['devicedata']['dburi']
+        client = MongoClient(uri)
         try:
             print(client.server_info())
-            #logger.info(client.server_info())
+            # logger.info(client.server_info())
         except ServerSelectionTimeoutError as err:
             logger.error("Could not connect to dataCache (uri = %s)" % uri)
-            raise err;
+            raise err
 
         return client
 
@@ -34,8 +34,8 @@ class DataCache:
         self.db=self.client['dataCache']
 
         ## create collection for device data
-        deviceData=self.db.create_collection(name='deviceData')
-        deviceData.create_index([("deviceType",1), ("deviceID",1),("timeUnit",1),("timeStamp", 1)], unique=True)
+        deviceData = self.db.create_collection(name='deviceData')
+        deviceData.create_index([("deviceType", 1), ("deviceID", 1),("timeUnit", 1),("timeStamp", 1)], unique=True)
 
         ## create collection for billing data
         self.db.create_collection(name='billingData')
@@ -43,23 +43,28 @@ class DataCache:
 
     def getDeviceList(self):
         deviceData = self.db['deviceData']
-        return deviceData.aggregate([{"$group": { "_id":{"deviceID":"$deviceID","deviceType":"$deviceType"}}}])
+        return deviceData.aggregate([{"$group": {"_id": {"deviceID": "$deviceID", "deviceType": "$deviceType"}}}])
 
-    def addDeviceDataSingleRecord(self,deviceType,deviceID, timeUnit,timeStamp, record):
+    def addDeviceDataSingleRecord(self, deviceType, deviceID, timeUnit, timeStamp, record):
         deviceData=self.db['deviceData']
         # TODO: appropriate error handling for duplicate data (catch exception?)
-        toInsert={"deviceType":deviceType, "deviceID":deviceID, "timeUnit":timeUnit,"timeStamp": timeStamp, "data":record}
-        deviceData.insert_one(toInsert)
+        to_insert = {"deviceType": deviceType,
+                     "deviceID": deviceID,
+                     "timeUnit": timeUnit,
+                     "timeStamp": timeStamp,
+                     "data": record}
+        deviceData.insert_one(to_insert)
 
     def addDeviceDataMultipleRecords(self, deviceType, deviceID, timeUnit, timeStampKey, records):
         for record in records:
-            timeStamp=record[timeStampKey]
-            self.addDeviceDataSingleRecord(deviceType,deviceID,timeUnit,timeStamp,record)
+            time_stamp = record[timeStampKey]
+            self.addDeviceDataSingleRecord(deviceType,deviceID,timeUnit,time_stamp,record)
 
     ##function to fetch deviceData for given timespan
     def getDeviceData(self, deviceType, deviceID, start, end, timeUnit):
-        deviceData=self.db['deviceData'].find({'deviceID':deviceID,  'deviceID':deviceID, 'timeUnit':timeUnit, 'timeStamp':{'$gte':start, '$lte':end}})
-        return [d['data'] for d in deviceData]
+        lookup_key = {'deviceID': deviceID, 'timeUnit': timeUnit, 'timeStamp': {'$gte': start, '$lte': end}}
+        device_data = self.db['deviceData'].find(lookup_key)
+        return [d['data'] for d in device_data]
 
     ##in principle, we should have a get-earliest timestamp too, but in the current model,
     ##  we are building up a history, so that seems unlikely to be needed
@@ -67,12 +72,12 @@ class DataCache:
         if self.db['deviceData'].count()==0:
             return minDate
 
-        filter={'deviceID':deviceID,  'deviceID':deviceID, 'timeUnit':timeUnit}
-        sort=[('timeStamp',pymongo.DESCENDING)]
-        queryResult=self.db['deviceData'].\
-            find_one(filter=filter, sort=sort,projection=['timeStamp'])['timeStamp']
+        filter = {'deviceID': deviceID, 'timeUnit': timeUnit}
+        sort = [('timeStamp', pymongo.DESCENDING)]
+        queryResult = self.db['deviceData'].\
+            find_one(filter=filter, sort=sort, projection=['timeStamp'])['timeStamp']
 
-        return max(minDate,queryResult)
+        return max(minDate, queryResult)
 
 
     #function to add billing address
