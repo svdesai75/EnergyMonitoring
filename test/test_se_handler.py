25f86f4c98fd0640a4aed29acc3e47a1dd12740f
satish <svdesai75@gmail.com>
2020-02-16 08:56:11 -0600

    Initial commit of unit test for SolarEdge handler

---
+++ test/test_se_handler.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from connectionModel import *
+from datetime import datetime, timedelta
+TimeZone = 'America/Chicago'
+
+engine = createDBEngine()
+session = dbConnect(engine)
+
+units = session.query(RentalUnit).all()
+lastUnit = units[-1]
+
+se = lastUnit.generationMonitor
+
+print(lastUnit.unitName)
+print(se)
+end_date = datetime.now()
+start_date = end_date - timedelta(days=1)
+print(start_date)
+print(end_date)
+print(se.download(start_date, end_date, 'HOUR'))
+
