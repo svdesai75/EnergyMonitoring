2c01e64473e2ba69df83dbfee19eabd25833123a
satish <svdesai75@gmail.com>
2022-07-16 09:12:12 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="EnergyMonitoring",
+    versiom="0.0.1",
+    author="Satish Desai",
+    description="Package to monitor energy usage and production",
+    author_email='svdesai75@gmail.com',
+    packages=["energyMonitoring"]
+)
